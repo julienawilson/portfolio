5174241880e1c7b2171098139fe83dfc2139a498
Julien Wilson <julienawilson@gmail.com>
2016-10-26 10:29:24 -0700

    added the article template and scripts to append the articles

---
+++ scripts/portfolioContent.js
@@ -0,0 +1,7 @@
+var portfolioContent = [
+  {title: 'Axis and Allies Battle Calculator',
+   body: 'The calculattor provides a probabilty distribution for different battle outcomes.'},
+   {title: 'Another Cool Thing I did!',
+    body: '<p>Experience here my very impressive work!</p>'}
+
+];
