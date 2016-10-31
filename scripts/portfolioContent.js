41f78749e61e2bc73941aee9c5c557f288b39903
Julien Wilson <julienawilson@gmail.com>
2016-10-31 11:52:47 -0700

    updated nav icon styling(color, positioning, removed underline), including recommendations from peer review

--- scripts/portfolioContent.js
+++ scripts/portfolioContent.js
@@ -4,14 +4,14 @@ var portfolioContent = [
    category: 'Games',
    image:'images/aanda.png'
   },
-   {title: 'Another Cool Thing I did!',
-    body: '<p>Experience here my very impressive work!</p>',
-    category: 'Other',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Sean_Connery_1971_(cropped).jpg'
-  },
   {title: 'FarmCloud GIS',
   body: 'Geospatial tech consulting for FarmCloud Inc.',
   category: 'GIS',
   image: 'http://nebula.wsimg.com/4676fb77559ade11afe3208999c25406?AccessKeyId=E1D99F0C4C7C61118992&disposition=0&alloworigin=1'
-  }
+},
+{title: 'Another Cool Thing I did!',
+ body: '<p>Experience here my very impressive work!</p>',
+ category: 'Other',
+ image: 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Sean_Connery_1971_(cropped).jpg'
+}
 ];
