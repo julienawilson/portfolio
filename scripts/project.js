8beaa479faa53c1e3ee3fcf304e4fb1175475a93
Julien Wilson <julienawilson@gmail.com>
2016-11-08 09:37:30 -0800

    changed names to be more relevant, add etag check before loading/getting new data

---
+++ scripts/project.js
@@ -0,0 +1,72 @@
+//IIFE
+(function(module) {
+  //make the new object
+  function Project (post){
+    for (key in post){
+      this[key] = post[key];
+    }
+  };
+
+  Project.allProjects = [];
+
+
+//grabs the template, inserts from object properties, creates html
+  Project.prototype.toHtml = function(scriptTemplateId) {
+    var template = Handlebars.compile($(scriptTemplateId).text());
+    return template(this);
+  };
+
+
+//takes input array and creates a new array of Project Objects
+  Project.loadAll = function(inputData){
+    Project.allProjects = inputData.map(function(elem){
+      return new Project(elem);
+    });
+  };
+
+  Project.getFromJSON = function(nextFunction){
+    $.getJSON('Data/portfolioContent.json', function(data, msg, xhr) {
+      console.log('data='+data);
+      Project.loadAll(data);
+      console.log('allproject='+Project.allProjects);
+      localStorage.blogProjects = JSON.stringify(data);
+      localStorage.ETag = xhr.getResponseHeader('ETag');
+      console.log('red');
+      nextFunction();
+    });
+  };
+
+
+//grabs data from JSON, calls loadAll() on it, then renderIndexPage()
+  Project.fetchAll = function(nextFunction){
+    if(localStorage.blogProjects){
+      $.ajax({
+        type: 'HEAD',
+        url:'Data/portfolioContent.json',
+        complete: function(xhr){
+          //console.log('I checked the etag');
+          if (localStorage.ETag||localStorage.ETag !== xhr.getResponseHeader('ETag')){
+            Project.getFromJSON(nextFunction);
+          }else{
+            Project.loadAll(JSON.parse(localStorage.blogProjects));
+            nextFunction();
+          }
+        }
+      });
+    }else{
+      Project.getFromJSON(nextFunction);
+    };
+  };
+
+
+//counts the number of projects
+  Project.numProjects=function(){
+    return Project.allProjects.reduce(function(acc, cur){
+      acc = acc + 1;
+      return acc;
+    },0);
+  };
+
+//END IFFE
+  module.Project = Project;
+})(window);
